8eb934cf22b933dd2b157876c49e0a783d988776
Tenebrosful <hugobernard39@gmail.com>
2021-12-06 04:00:53 +0100

    Add errorHandlers

--- src/api/server.js
+++ src/api/server.js
@@ -9,6 +9,12 @@ import logger from "./middleware/logger.js";
 
 app.use(logger);
 
+import error400 from "./errors/error400.js";
+import error500 from "./errors/error500.js";
+
+app.use(error400);
+app.use(error500);
+
 app.listen(process.env.expressPort || 3000, () => {
   console.log(`Listening at http://localhost:${process.env.expressPort || 3000}`);
 });
