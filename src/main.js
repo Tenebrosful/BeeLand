e151ef9f457d79ee815cf8de579bdd6168386962
SpilmontFrancois <francois.spilmont5@etu.univ-lorraine.fr>
2022-01-14 17:34:47 +0100

    fix(app): fix "cleaning vue install"

--- src/main.js
+++ src/main.js
@@ -1,4 +1,10 @@
 import { createApp } from 'vue'
-import App from './App.vue'
+import App from './index.vue'
 
-createApp(App).mount('#app')
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap-vue/dist/bootstrap-vue.css"
+
+createApp(App).use(VueAxios, axios).mount('#app')
