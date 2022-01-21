f856b0ee5c7f140482ba9eb4276cde9f4f282276
SpilmontFrancois <francois.spilmont5@etu.univ-lorraine.fr>
2022-01-21 17:13:43 +0100

    wip: get localisation of plants

--- src/pages/map/index.vue
+++ src/pages/map/index.vue
@@ -32,6 +32,82 @@ export default {
       const { data } = await this.axios.get(process.env.VUE_APP_SERVER_URL);
       console.log(data);
     },
+    getPositionByPlaceName(place) {
+      let { lat, lng } = null;
+      switch (place) {
+        case "collection historique":
+          // 48.661535, 6.154234
+          lat = 48.661535;
+          lng = 6.154234;
+          break;
+        case "forêt":
+          // 48.659767, 6.156138
+          lat = 48.659767;
+          lng = 6.156138;
+          break;
+        case "étang":
+          // 48.660104, 6.152930
+          lat = 48.660104;
+          lng = 6.15293;
+          break;
+        case "collection ornementale":
+          // 48.661897, 6.154988
+          lat = 48.661897;
+          lng = 6.154988;
+          break;
+        case "secteur arboreteum Chine":
+          // 48.662623, 6.157450
+          lat = 48.662623;
+          lng = 6.157450;
+          break;
+        case "secteur terre de bruyère":
+          // 48.659029, 6.154923
+          lat = 48.659029;
+          lng = 6.154923;
+          break;
+        case "secteur arboreteum Europe":
+          // 48.661729, 6.156998
+          lat = 48.661729;
+          lng = 6.156998;
+          break;
+        case "arboretum, secteur Chine":
+          // 48.662623, 6.157450
+          lat = 48.662623;
+          lng = 6.157450;
+          break;
+        case "pelouse":
+          // 48.659955, 6.156835
+          lat = 48.659955;
+          lng = 6.156835;
+          break;
+        case "collection de dahlias":
+          // 
+          lat = 48.659955;
+          lng = 6.156835;
+          break;
+        case "ruisseau":
+          break;
+        case "vert tendre, forêt":
+          break;
+        case "potager":
+          break;
+        case "potager écologique":
+          break;
+        case "roseraie":
+          break;
+        case "étang pavillon d'accueil":
+          break;
+        case "pelouse praierie":
+          break;
+        case "jardin écologique, potager":
+          break;
+        case "parcelle Emile Gallé":
+          break;
+        case "arboretum":
+          break;
+      }
+      return { lat, lng };
+    },
   },
 };
 </script>
