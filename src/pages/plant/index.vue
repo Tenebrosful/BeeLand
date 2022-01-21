15121e65b4afb1ca75df6c35d6d5c9d04034d9e2
Ziyi WANG <ziyiwang1027@gmail.com>
2022-01-21 16:25:44 +0100

    add page plant

---
+++ src/pages/plant/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <router-link
+      class="fixed-top card backButton d-flex justify-content-center"
+      title="Retour à l'accueil"
+      to="/"
+    >
+      <img
+        class="w-100"
+        src="../../assets/icons/arrow-left-solid.svg"
+        height="40"
+        width="40"
+      />
+    </router-link>
+    <Map />
+  </div>
+</template>
+
+<script>
+import Map from "../../components/Map";
+
+export default {
+  components: {
+    Map,
+  },
+};
+</script>
